cd34b4997d0d9a06bb9069aff0a92c4ecf57f122
Thomas.Okonkwo <thomas.okonkwo@interswitchgroup.com>
2020-06-27 18:47:34 +0100

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,7 +15,7 @@ function toggleMenu(){
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('close');
+        menuBranding.classList.add('show');
         navItems.forEach(item=> item.classList.add('show'));
         //set menu state
         showMenu=true;
@@ -24,7 +24,7 @@ function toggleMenu(){
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('close');
+        menuBranding.classList.remove('show');
         navItems.forEach(item=> item.classList.remove('show'));
         //set menu state
         showMenu=false;
